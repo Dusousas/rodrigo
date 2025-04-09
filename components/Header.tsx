56dcc0e35f93c28f1a7123b92491f8af974f08d3
Dusousas <dusousadeveloper@gmail.com>
2025-04-09 17:08:18 -0300

    logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,6 @@ export default function Header() {
       <section className='py-6 px-2'>
         <div className='maxW flex justify-between items-center'>
           <h1 className='text-white uppercase text-2xl font-semibold tracking-wider'>Café Logo</h1>
-
           <Navbar />
         </div>
       </section>
